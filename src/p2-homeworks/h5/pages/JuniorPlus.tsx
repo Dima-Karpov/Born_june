e3871b59b3a9050a9319bdd42e5b16c3a007c930
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-07-24 23:08:11 +0300

    '1/2'

--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Error404 from './Error404';
 
 export const JuniorPlus = () => {
     return (
         <div>
-            <Error404/>
+           
         </div>
     );
 }
